ed7fbca63668a55daea5f5b9c5169123e260db07
Domain name System <dns@Domains-MacBook-Air.local>
2024-09-30 18:25:52 -0300

    chore: Aula tres

---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "region" {
+  
+}
+
+variable "service_name" {
+}
+
+variable "cluster_name" {
+}
+
+variable "vpc_id" {
+}
+
+variable "private_subnet" {
+}
+
+variable "service_port" {
+}
+
+variable "service_cpu" {
+}
+
+variable "service_memory" {
+}
+
+variable "ssm_listener" {
+}
+
+variable "ssm_vpc_id" {
+  
+}
+
+variable "ssm_private_subnet_1" {
+  
+}
+
+variable "ssm_private_subnet_2" {
+  
+}
+
+variable "ssm_private_subnet_3" {
+  
+}
